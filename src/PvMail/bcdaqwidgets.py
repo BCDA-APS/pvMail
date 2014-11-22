d2db4ab35e7d1ac40ea6cb75ed692447bb0b0d86
Pete Jemian <prjemian@gmail.com>
2014-11-22 07:13:33 -0600

    good example for the bcdaqwidgets

--- src/PvMail/bcdaqwidgets.py
+++ src/PvMail/bcdaqwidgets.py
@@ -27,6 +27,43 @@ widget                         description
        of the Advanced Photon Source, Argonne National Laboratory,
        http://www.aps.anl.gov/bcda
 
+EXAMPLE:
+
+Here is a complete example program for use with the test.db database:
+
+.. code-block:: python
+   :linenos:
+
+    try:
+        from PyQt4 import QtCore, QtGui
+        pyqtSignal = QtCore.pyqtSignal
+    except:
+        from PySide import QtCore, QtGui
+        pyqtSignal = QtCore.Signal
+    import sys
+    import bcdaqwidgets
+    
+    class DemoView(QtGui.QWidget):
+    
+        def __init__(self, parent=None):
+            QtGui.QWidget.__init__(self, parent)
+            
+            layout = QtGui.QFormLayout()
+            self.setLayout(layout)
+            
+            lbl = QtGui.QLabel('trigger')
+            pb = bcdaqwidgets.BcdaQToggleButton('pvMail:trigger')
+            layout.addRow(lbl, pb)
+            
+            lbl = QtGui.QLabel('message')
+            msg = bcdaqwidgets.BcdaQLineEdit('pvMail:message')
+            layout.addRow(lbl, msg)
+    
+    app = QtGui.QApplication(sys.argv)
+    view = DemoView()
+    view.show()
+    sys.exit(app.exec_())
+
 '''
 
 
