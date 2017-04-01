1b0d0acac179f4a7d0b95a6b34720835480a6f70
Pete Jemian <prjemian@gmail.com>
2017-04-01 11:45:50 -0500

    refs #14

--- setup.py
+++ setup.py
@@ -5,6 +5,7 @@
 from setuptools import setup, find_packages
 import os
 import sys
+import versioneer
 
 # make sure our development source is found FIRST!
 sys.path.insert(0, os.path.abspath('./src'))
@@ -12,7 +13,8 @@ import PvMail
 
 setup(
     name             = 'PvMail',
-    version          = PvMail.__version__,
+    version          = versioneer.get_version(),
+    cmdclass         = versioneer.get_cmdclass(),
     author           = PvMail.__author__,
     author_email     = PvMail.__author_email__,
     url              = PvMail.__url__,
@@ -24,14 +26,17 @@ setup(
     platforms        = 'any',
     install_requires = PvMail.__install_requires__,
     packages         = ['PvMail',],
-    package_dir      = {'PvMail': 'src/PvMail',},
-    package_data     = {'PvMail': [
-                                    'test.db', 
-                                    'resources/*',
-                                    'LICENSE', 
-                                    'VERSION', 
-                                   ],
-                        },
+    package_dir      = {
+        'PvMail': 'src/PvMail',
+    },
+    package_data     = {
+        'PvMail': [
+            'test.db', 
+            'resources/*',
+            'LICENSE', 
+            'VERSION', 
+           ],
+    },
     entry_points     = {
         # create & install console_scripts in <python>/bin
         'console_scripts': PvMail.__console_scripts__,
--- src/PvMail/__init__.py
+++ src/PvMail/__init__.py
@@ -1,22 +1,4 @@
 import os
-from ._version import git_release
-
-
-VERSION_FILE = 'VERSION'
-
-
-def get_pkg_file_path(fname):
-    '''return the absolute path to the named file in the package directory'''
-    vpath = os.path.abspath(os.path.dirname(__file__))
-    vfile = os.path.join(vpath, fname)
-    if os.path.exists(vfile):
-        RuntimeError('file does not exist: ' + vfile)
-    return vfile
-
-
-def read_resource_file(fname):
-    '''return contents of the named file in the package directory'''
-    return open(get_pkg_file_path(fname)).read()
 
 
 __classifiers__ = [
@@ -37,8 +19,6 @@ __install_requires__ = ['pyepics>=3.2.3', ]
 
 __project_name__ = "PvMail"
 __description__ = "Watch an EPICS PV. Send email when it changes from 0 to 1."
-__version__ = read_resource_file(VERSION_FILE).strip()
-__release__ = git_release(__project_name__, __version__)
 __author__ = "Pete Jemian"
 __full_author_list__ = ["Pete Jemian", "Kurt Goetze"]
 __institution__ = "Advanced Photon Source, Argonne National Laboratory"
@@ -49,7 +29,12 @@ __license__ += " (see LICENSE file for details)"
 
 # create & install console_scripts in <python>/bin
 __console_scripts__ = [
-                       'pvMail = PvMail.pvMail:main', 
+                       'pvMail = PvMail.cli:main', 
                        'pvMail_mail_test = PvMail.mailer:main', 
                        'pvMail_mail_config_file = PvMail.ini_config:main', 
                        ]
+
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
